2bd6c5c48bbaa910360019761d320c3c208841d1
Juampatagle <juampatagle@gmail.com>
2021-03-18 13:14:23 -0300

    agregue html

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 const express = require('express');
 const app = express();
+
+app.listen(3030,()=>{
+console.log('Servidor corriendo');
+})
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/prueba.html');
+});
+
+app.get('/Productos', function(req, res){
+    res.send('Estos son nuestros productos');
+});
+
+app.get('/contacto', function(req, res){
+    res.send('Cualquier problema o duda comunicarse a este numero 47922148')
+})
